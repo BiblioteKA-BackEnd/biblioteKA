02e492c25c9c669aff00f0e0f897becc0037f2ca
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-05-03 13:37:59 -0300

    models

--- lending/models.py
+++ lending/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Lending(models.Model):
+    copy_id = models.IntegerField(null=True)
+    created_at = models.DateTimeField(null=True)
+    devolution_at = models.DateTimeField(null=True)
+    ...
